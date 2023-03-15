ffdca13f06c44150cca122b206fed535b92e257e
Piotr Szydlowski <pszydlowski@listonic.com>
2023-03-15 14:14:50 +0100

    poprawka koloru androidowych ikon

--- app/src/main/java/szydlowskiptr/com/epz/activity/HomeFragment.java
+++ app/src/main/java/szydlowskiptr/com/epz/activity/HomeFragment.java
@@ -1,15 +1,16 @@
 package szydlowskiptr.com.epz.activity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import static android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS;
+
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
 
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -29,6 +30,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getActivity().getWindow().getDecorView().getWindowInsetsController().setSystemBarsAppearance(APPEARANCE_LIGHT_STATUS_BARS, APPEARANCE_LIGHT_STATUS_BARS);
+        }
 
         sliderView = view.findViewById(R.id.image_slider);
         searchView = view.findViewById(R.id.search_main);
@@ -39,7 +43,18 @@ public class HomeFragment extends Fragment {
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
 
+        clickSearchMain();
 
         return view;
+    }
+
+    public void clickSearchMain() {
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), SearchActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
