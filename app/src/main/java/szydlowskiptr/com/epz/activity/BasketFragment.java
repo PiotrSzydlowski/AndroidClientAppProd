e1cb1cc2d505c40981503b106c41f8726e1d4b7c
Piotr Szydlowski <pszydlowski@listonic.com>
2023-03-17 16:11:50 +0100

    basket fragment

--- app/src/main/java/szydlowskiptr/com/epz/activity/BasketFragment.java
+++ app/src/main/java/szydlowskiptr/com/epz/activity/BasketFragment.java
@@ -1,20 +1,43 @@
 package szydlowskiptr.com.epz.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import szydlowskiptr.com.epz.R;
 
 public class BasketFragment extends Fragment {
 
+    Button startShoppingBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_basket, container, false);
+        View view = inflater.inflate(R.layout.fragment_basket, container, false);
+
+        startShoppingBtn = view.findViewById(R.id.start_shopping_btn_cart);
+        clickStartShoppingBtn();
+        return view;
+    }
+
+    private void clickStartShoppingBtn() {
+        startShoppingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.basket_fragment, new HomeFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
     }
 }
