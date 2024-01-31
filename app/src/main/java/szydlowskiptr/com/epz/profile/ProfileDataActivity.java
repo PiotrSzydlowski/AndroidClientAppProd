07933b8ee4d5891465fa6bdb88383e31a805d895
psz <piotr.szydlowski@kep.pl>
2024-01-31 20:52:26 +0100

    initial commit

--- app/src/main/java/szydlowskiptr/com/epz/profile/ProfileDataActivity.java
+++ app/src/main/java/szydlowskiptr/com/epz/profile/ProfileDataActivity.java
@@ -1,10 +1,13 @@
 package szydlowskiptr.com.epz.profile;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import szydlowskiptr.com.epz.Helper.PrefConfig;
 import szydlowskiptr.com.epz.R;
 import szydlowskiptr.com.epz.databinding.ActivityCheckoutBinding;
 import szydlowskiptr.com.epz.databinding.ActivityProfileDataBinding;
@@ -20,5 +23,37 @@ public class ProfileDataActivity extends AppCompatActivity {
         binding = ActivityProfileDataBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        PrefConfig.registerPref(getApplicationContext());
+        saveProfileData();
+        deleteAccount();
+
+        
     }
+
+    private void saveProfileData() {
+        binding.idBtnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userId = PrefConfig.loadUserIdFromPref(getApplicationContext());
+                Editable emailTextView = binding.emailEdittext.getText();
+                Editable birthdayText = binding.birthday.getText();
+                Editable phoneEdittextText = binding.phoneEdittext.getText();
+
+                if (!emailTextView.equals(null) ||!birthdayText.equals(null) || !phoneEdittextText.equals(null)){
+                    Toast.makeText(getApplicationContext(), PrefConfig.loadUserIdFromPref(getApplicationContext()), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void deleteAccount(){
+        binding.idBtnDeleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
 }
